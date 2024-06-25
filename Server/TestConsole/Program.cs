a2b6df80779abcfe84bc7f65eba2cce11a2d814e
gaven <gavenf2023@outlook.com>
2024-06-25 11:13:55 +0800

    s: MemoryCache服务测试

--- Server/TestConsole/Program.cs
+++ Server/TestConsole/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 
-var GF_WEB_LOGO = """
+var logo = """
     -------------------------------------------
      ██████╗ ███████╗██╗    ██╗███████╗██████╗ 
     ██╔════╝ ██╔════╝██║    ██║██╔════╝██╔══██╗
@@ -13,40 +13,71 @@ var GF_WEB_LOGO = """
     -------------------------------------------
     """;
 
-foreach (var @char in GF_WEB_LOGO)
+logo.ToList().ForEach(c =>
 {
-    Console.Write(@char);
-}
-
+    Console.Write(c);
+    Task.Delay(1).Wait();
+});
 Console.WriteLine();
+
 Console.WriteLine("这只是一个测试使用的控制台，代码逻辑测试");
 
 var service = new ServiceCollection();
-service.AddStackExchangeRedisCache(
-    (options) =>
-    {
-        options.Configuration = "s4.serv00.com:63857,password=GY00..yl";
-        options.InstanceName = "DEMO_";
-    }   
-);
+
+service.AddMemoryCache();
 
 var serviceCollection = service.BuildServiceProvider();
 
-//var redis = serviceCollection.GetService<IDistributedCache>();
-var redis = serviceCollection.GetService<IDistributedCache>();
-var data1 = redis?.GetString("Date1");
-var data2 = redis?.GetString("data2");
+var cache = serviceCollection.GetService<IMemoryCache>();
 
+var data1 = cache!.GetOrCreate("KEY1", (cacheEntity) =>
+{
+    Console.WriteLine("Create Key 1");
+    cacheEntity.SlidingExpiration = TimeSpan.FromSeconds(30);
+    return "Data01";
+});
 
-redis?.SetString(
-    "data1",
-    "Data",
-    new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(1) }
-);
-redis?.SetString(
-    "data2",
-    "Data2",
-    new DistributedCacheEntryOptions() { SlidingExpiration = TimeSpan.FromSeconds(30) }
-);
+var data2 = cache!.GetOrCreate("KEY1", (cacheEntity) =>
+{
+    Console.WriteLine("Create Key 1");
+    cacheEntity.SlidingExpiration = TimeSpan.FromSeconds(30);
+    return "Data01";
+});
 
+Console.WriteLine(data1);
 Console.WriteLine(data2);
+
+Console.WriteLine(data2!.Equals(data1));
+
+
+
+#region Redis Service
+//service.AddStackExchangeRedisCache(
+//    (options) =>
+//    {
+//        options.Configuration = "s4.serv00.com:63857,password=GY00..yl";
+//        options.InstanceName = "DEMO_";
+//    }
+//);
+
+//var serviceCollection = service.BuildServiceProvider();
+
+////var redis = serviceCollection.GetService<IDistributedCache>();
+//var redis = serviceCollection.GetService<IDistributedCache>();
+//var data1 = redis?.GetString("Date1");
+//var data2 = redis?.GetString("data2");
+
+
+//redis?.SetString(
+//    "data1",
+//    "Data",
+//    new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(1) }
+//);
+//redis?.SetString(
+//    "data2",
+//    "Data2",
+//    new DistributedCacheEntryOptions() { SlidingExpiration = TimeSpan.FromSeconds(30) }
+//);
+
+//Console.WriteLine(data2);
+#endregion
