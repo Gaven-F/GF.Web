fdf8be5a1b9067a04c6d76d0eb377e6d8e9cc599
gaven <gavenf2023@outlook.com>
2024-06-27 18:25:26 +0800

    g: 格式化文档

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,35 +1,37 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(c =>
-{
-    
-});
-builder.Services.AddOpenApiDocument((c) =>
-{
-    c.PostProcess = p =>
+builder.Services.AddControllers(c => { });
+builder.Services.AddOpenApiDocument(
+    (c) =>
     {
-        p.Info.Title = "GF.Web";
-        p.Info.Version = "¦Á.0.1";
-        p.Info.Contact = new NSwag.OpenApiContact()
+        c.PostProcess = p =>
         {
-            Name = "GavenF",
-            Email = "gavenf2023@outlook.com",
+            p.Info.Title = "GF.Web";
+            p.Info.Version = "¦Á.0.1";
+            p.Info.Contact = new NSwag.OpenApiContact()
+            {
+                Name = "GavenF",
+                Email = "gavenf2023@outlook.com",
+            };
+            //p.Info.License = new NSwag.OpenApiLicense()
+            //{
+            //    Name = "MIT",
+            //};
         };
-        //p.Info.License = new NSwag.OpenApiLicense()
-        //{
-        //    Name = "MIT",
-        //};
-    };
-    c.DocumentName = "Default";
-    c.Title = "GF.Web";
-    c.ApiGroupNames = ["Default"];
-});
+        c.DocumentName = "Default";
+        c.Title = "GF.Web";
+        c.ApiGroupNames = ["Default"];
+    }
+);
 
 var app = builder.Build();
 
 app.UseRouting();
 app.UseOpenApi(c => { });
-app.UseSwaggerUi(c => { c.Path = ""; });
+app.UseSwaggerUi(c =>
+{
+    c.Path = string.Empty;
+});
 app.MapControllers();
 
 app.Run();
