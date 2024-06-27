90bfd8e385d4bb8064b4ade0ddbabcdcd3af0185
gaven <gavenf2023@outlook.com>
2024-06-27 18:21:06 +0800

    s: 创建基本服务端
    创建测试接口；
    创建Swagger文档；

---
+++ Server/Server/Controllers/BackstageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("BS/[action]")]
+[EndpointGroupName("Default")]
+public class BackstageController : ControllerBase
+{
+    public ActionResult Index()
+    {
+        return Ok("ok");
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(c =>
+{
+    
+});
+builder.Services.AddOpenApiDocument((c) =>
+{
+    c.PostProcess = p =>
+    {
+        p.Info.Title = "GF.Web";
+        p.Info.Version = "¦Á.0.1";
+        p.Info.Contact = new NSwag.OpenApiContact()
+        {
+            Name = "GavenF",
+            Email = "gavenf2023@outlook.com",
+        };
+        //p.Info.License = new NSwag.OpenApiLicense()
+        //{
+        //    Name = "MIT",
+        //};
+    };
+    c.DocumentName = "Default";
+    c.Title = "GF.Web";
+    c.ApiGroupNames = ["Default"];
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseOpenApi(c => { });
+app.UseSwaggerUi(c => { c.Path = ""; });
+app.MapControllers();
+
+app.Run();
