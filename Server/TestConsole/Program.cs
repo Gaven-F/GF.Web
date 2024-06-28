79fe17d8fdad64bbe8786fc0282827c954bef1ad
gaven <gavenf2023@outlook.com>
2024-06-28 23:01:58 +0800

    s: 结果测试可以嵌套深层次
    尝试优化获取配置逻辑

--- Server/TestConsole/Program.cs
+++ Server/TestConsole/Program.cs
@@ -1,106 +1,21 @@
-﻿#pragma warning disable CA2208
-
-using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-var logo = """
-    -------------------------------------------
-     ██████╗ ███████╗██╗    ██╗███████╗██████╗ 
-    ██╔════╝ ██╔════╝██║    ██║██╔════╝██╔══██╗
-    ██║  ███╗█████╗  ██║ █╗ ██║█████╗  ██████╔╝
-    ██║   ██║██╔══╝  ██║███╗██║██╔══╝  ██╔══██╗
-    ╚██████╔╝██║██╗  ╚███╔███╔╝███████╗██████╔╝
-     ╚═════╝ ╚═╝╚═╝   ╚══╝╚══╝ ╚══════╝╚═════╝ 
-    -------------------------------------------
-    """;
-
-logo.ToList()
-    .ForEach(c =>
-    {
-        Console.Write(c);
-        Task.Delay(0).Wait();
-    });
-Console.WriteLine();
-
-Console.WriteLine("这只是一个测试使用的控制台，代码逻辑测试");
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json.Serialization;
 
 var conf = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.local.json")
     .Build();
 
-var service = new ServiceCollection();
+var data = new DemoOptions();
 
-service.AddStackExchangeRedisCache(
-    (options) =>
-    {
-        var redisConf = conf.GetSection("Redis") ?? throw new ArgumentNullException("redisConfig");
+conf.GetSection(nameof(DemoOptions)).Bind(data);
 
-        options.InstanceName = "DEMO_";
-        options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
-        {
-            EndPoints = { { redisConf["Host"]!, Convert.ToInt32(redisConf["Port"]) } },
-            Password = redisConf["Password"]!,
-            DefaultDatabase = Convert.ToInt32(redisConf["DefaultDatabase"]),
-        };
-    }
-);
+Console.WriteLine(data.SubOption?.SubOption?.SubOption?.SubOption?.DemoData);
 
-service.AddMemoryCache();
+public class DemoOptions
+{
+    [ConfigurationKeyName("Demo")]
+    public string DemoData { get; set; } = string.Empty;
 
-#region Memory Cacge Service
-
-var serviceCollection = service.BuildServiceProvider();
-
-var cache = serviceCollection.GetService<IMemoryCache>();
-
-var data1 = cache!.GetOrCreate(
-    "KEY1",
-    (cacheEntity) =>
-    {
-        Console.WriteLine("Create Key 1");
-        cacheEntity.SlidingExpiration = TimeSpan.FromSeconds(30);
-        return "Data01";
-    }
-);
-
-var data2 = cache!.GetOrCreate(
-    "KEY1",
-    (cacheEntity) =>
-    {
-        Console.WriteLine("Create Key 1");
-        cacheEntity.SlidingExpiration = TimeSpan.FromSeconds(30);
-        return "Data01";
-    }
-);
-
-Console.WriteLine(data1);
-Console.WriteLine(data2);
-
-Console.WriteLine(data2!.Equals(data1));
-#endregion
-
-
-
-#region Redis Service
-
-//var redis = serviceCollection.GetService<IDistributedCache>();
-var redis = serviceCollection.GetService<IDistributedCache>();
-var r_data1 = redis?.GetString("Date1");
-var r_data2 = redis?.GetString("data2");
-
-redis?.SetString(
-    "Data1",
-    "Data",
-    new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(1) }
-);
-redis?.SetString(
-    "Data2",
-    "Data2",
-    new DistributedCacheEntryOptions() { SlidingExpiration = TimeSpan.FromSeconds(30) }
-);
-
-Console.WriteLine(r_data2);
-#endregion
+    public DemoOptions? SubOption { get; set; }
+}
